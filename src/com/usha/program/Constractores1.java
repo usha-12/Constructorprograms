b88616fc7f750ac0d7956a2182b29f7010e6913c
usha-12 <usha12ahirwar@gmail.com>
2022-10-24 16:06:36 +0530

    basic program null constractor

--- src/com/usha/program/Constractores1.java
+++ src/com/usha/program/Constractores1.java
@@ -1,4 +1,15 @@
 package com.usha.program;
 
 public class Constractores1 {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.age = 10;// one way of assigning value with the help of constructor 
+        System.out.println(d1.age);
+        d1.breed = "rhjjj";
+        System.out.println(d1.breed);
+        d1.color = "black";
+        System.out.println(d1.color);
+        System.out.println(d1.VOICE);
+
+    }
 }
--- src/com/usha/program/Dog.java
+++ src/com/usha/program/Dog.java
@@ -1,2 +1,21 @@
-package com.usha.program;public class Dog {
+package com.usha.program;
+
+public class Dog {
+    String breed;
+    int age;
+    String color;
+    public Dog(){
+        System.out.println("Dog() constructor");
+    }
+    static final String VOICE = "barks";
+
+    public static void main(String[] args) {
+        Dog d2 = new Dog();
+        System.out.println(d2.breed);
+        System.out.println(d2.color);
+        System.out.println(d2.age);
+        System.out.println(d2.VOICE);
+
+
+    }
 }
